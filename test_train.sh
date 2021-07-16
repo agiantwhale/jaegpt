b627385a03818ebfe3a4ab02c2489ff2f146bace
Il Jae Lee <agiantwhale@gmail.com>
2021-07-16 10:31:58 -0700

    Updated search files

--- test_train.sh
+++ test_train.sh
@@ -1,10 +1,14 @@
 rm -rf models tensorboard $HOME/.cache/huggingface/datasets
 
 python run_clm.py \
-    --train_file ./fb_data/small_train.json \
-    --validation_file ./fb_data/small_test.json \
-    --block_size 50 \
-    --output_dir ./models \
+    --train_file ./fb_data/sanitized/fb_train.json \
+    --validation_file ./fb_data/sanitized/fb_test.json \
+    --block_size 128 \
+    --output_dir ./models/test \
     --overwrite_cache true \
     --do_train \
-    --do_eval
+    --do_eval \
+    --max_steps 10 \
+    --report_to none \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 4
